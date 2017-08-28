da24d0f4d58160d4d05ebf15ce389fdcadb00446
Alan <alanwei000@gmail.com>
2017-08-28 15:51:44 +0800

    add test case

---
+++ index.spec.ts
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+import './index.ts';
+
+test('1. base', () => {
+  const arr = _.chain([1, 3, 5])
+    .map(v => v * 2)
+    .log('new array: ')
+    .map(v => v + 1)
+    .value();
+
+  expect(arr).toEqual([3, 7, 11]);
+});
+
+test('2. format', () => {
+  const v = _.chain([1, 3, 5])
+    .reduce((result: number, current) => result + current)
+    .log('sum: %d')
+    .isNumber()
+    .value();
+
+  expect(v).toBe(true);
+});
